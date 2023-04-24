4bb486e273da8630a6befb65058f1c84252f8f8d
timdev0 <quentin.peltier.pro@gmail.com>
2023-04-24 20:43:19 +0200

    feat(tests-items): add itesm

---
+++ src/item.js
@@ -0,0 +1,7 @@
+class Item {
+  constructor(name) {
+    this.name = name;
+    this.date = new Date();
+  }
+}
+export default Item;
--- test/todoList.mjs
+++ test/todoList.mjs
@@ -3,6 +3,7 @@ import assert from 'node:assert';
 
 import User from './../src/user.js';
 import TodoList from './../src/todoList.js';
+import Item from './../src/todoList.js';
 
 const user = new User('John', 'Doe', 'zabuza@gmail.com', new Date('1990-01-01'), 'Password123');
 const todoList = new TodoList('My todo list');
@@ -39,4 +40,49 @@ describe('Removing a todo list from the user', () => {
   it('should throw an error if the user don\'t have any todo list', () => {
     assert.throws(() => {user.removeTodoList(todoList)});
   });
+});
+
+// items
+describe('add items to todo list', () => {
+  it('should return true if the item is added', () => {
+    const item = new Item("one", new Date());
+    const result = todoList.addItem(item);
+
+    assert.strictEqual(result, true);
+  });
+
+  it('should return false if the name is not unique', () => {
+    const item = new Item("My first item", new Date());
+    // todolist should be populated with an item with the same name
+    // or
+    // const secondItem = new Item("My first item", new Date());
+    // todoList.addItem(secondItem);
+
+    const result = todoList.addItem(item);
+
+    assert.strictEqual(result, false);
+  });
+
+  it('should return false if the name is more than 1000 characters', () => {
+    const item = new Item("a".repeat(1001));
+    const result = todoList.addItem(item);
+
+    assert.strictEqual(result, false);
+  });
+
+  it('should return false if last two items had been added less than 30min ago', () => {
+    const item = new Item("one", new Date());
+    const itemTwo = new Item("one", new Date());
+    const itemThree = new Item("one", new Date());
+
+    todoList.addItem(item);
+    todoList.addItem(itemTwo);
+    const result = todoList.addItem(itemThree);
+
+    assert.strictEqual(result, false);
+  });
+
+  it('should return false if the user is not valid', () => {
+  });
+
 });
