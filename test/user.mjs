bd9969ced88cfc2e639b677dab177a33342f3c2f
mrpinkcat <gatien.anizan@gmail.com>
2023-04-26 00:08:00 +0200

    refacto(tests): use destructuring for assert import

--- test/user.mjs
+++ test/user.mjs
@@ -1,23 +1,23 @@
-import { describe, it } from 'node:test';
-import assert from 'node:assert';
+import { describe, it, mock } from 'node:test';
+import { doesNotThrow, throws } from 'node:assert';
 
 import User from '../src/user.js';
 
 describe('User creation', () => {
   it('should instantiate a new User if the user is valid', () => {
-    assert.doesNotThrow(() => {
+    doesNotThrow(() => {
       new User('John', 'Doe', 'zabuza@gmail.com', new Date(new Date().setFullYear(new Date().getFullYear() - 30)), 'Password123');
     });
   });
 
   it('should instantiate a new User if the user exactly 13yo', () => {
-    assert.doesNotThrow(() => {
+    doesNotThrow(() => {
       new User('John', 'Doe', 'zabuza@gmail.com', new Date(new Date().setFullYear(new Date().getFullYear() - 13)), 'Password123');
     });
   });
   
   it('shouldn\'t instantiate a new User if the user is under 13yo', () => {
-    assert.throws(
+    throws(
       () => {
         new User('John', 'Doe', 'zabuza@gmail.com', new Date(new Date().setFullYear(new Date().getFullYear() - 12)), 'Password123');
       },
@@ -26,7 +26,7 @@ describe('User creation', () => {
   });
   
   it('shouldn\'t instantiate a new User if the user\'s firstname is too short', () => {
-    assert.throws(
+    throws(
       () => {
         new User('J', 'Doe', 'zabuza@gmail.com', new Date('1990-01-01'), 'Password123');
       },
@@ -35,7 +35,7 @@ describe('User creation', () => {
   });
   
   it('shouldn\'t instantiate a new User if the user\'s firstname is too long', () => {
-    assert.throws(
+    throws(
       () => {
         new User('JhonJhonJhonJhonJhonJhon', 'Doe', 'zabuza@gmail.com', new Date('1990-01-01'), 'Password123');
       },
@@ -44,7 +44,7 @@ describe('User creation', () => {
   });
   
   it('shouldn\'t instantiate a new User if the user\'s lastname is too short', () => {
-    assert.throws(
+    throws(
       () => {
         new User('Jhon', 'D', 'zabuza@gmail.com', new Date('1990-01-01'), 'Password123');
       },
@@ -53,7 +53,7 @@ describe('User creation', () => {
   });
   
   it('shouldn\'t instantiate a new User if the user\'s lastname is too long', () => {
-    assert.throws(
+    throws(
       () => {
         new User('Jhon', 'DoeDoeDoeDoeDoeDoeDoe', 'zabuza@gmail.com', new Date('1990-01-01'), 'Password123');
       },
@@ -62,7 +62,7 @@ describe('User creation', () => {
   });
   
   it('shouldn\'t instantiate a new User if the user\'s email is not valid', () => {
-    assert.throws(
+    throws(
       () => {
         new User('Jhon', 'Doe', 'zabuza', new Date('1990-01-01'), 'Password123');
       },
@@ -71,7 +71,7 @@ describe('User creation', () => {
   });
   
   it('shouldn\'t instantiate a new User if the user\'s password is too short', () => {
-    assert.throws(
+    throws(
       () => {
         new User('Jhon', 'Doe', 'zabuza@gmail.com', new Date('1990-01-01'), 'Pa1');
       },
@@ -80,7 +80,7 @@ describe('User creation', () => {
   });
   
   it('shouldn\'t instantiate a new User if the user\'s password is too long', () => {
-    assert.throws(
+    throws(
       () => {
         new User('Jhon', 'Doe', 'zabuza@gmail.com', new Date('1990-01-01'), 'Password123Password123Password123Password123');
       },
@@ -89,7 +89,7 @@ describe('User creation', () => {
   });
   
   it('shouldn\'t instantiate a new User if the user\'s password not containing a lowercase letter', () => {
-    assert.throws(
+    throws(
       () => {
         new User('Jhon', 'Doe', 'zabuza@gmail.com', new Date('1990-01-01'), 'PASSWORD123');
       },
@@ -98,7 +98,7 @@ describe('User creation', () => {
   });
   
   it('shouldn\'t instantiate a new User if the user\'s password not containing a uppercase', () => {
-    assert.throws(
+    throws(
       () => {
         new User('Jhon', 'Doe', 'zabuza@gmail.com', new Date('1990-01-01'), 'password123');
       },
@@ -107,7 +107,7 @@ describe('User creation', () => {
   });
   
   it('shouldn\'t instantiate a new User if the user\'s password not containing a number', () => {
-    assert.throws(
+    throws(
       () => {
         new User('Jhon', 'Doe', 'zabuza@gmail.com', new Date('1990-01-01'), 'Password');
       },
